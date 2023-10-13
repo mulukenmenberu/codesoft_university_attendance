01a0ed93433dc008b938dd3730424f103738535f
Muluken <mulerapps@gmail.com>
2023-10-13 15:37:37 +0300

    dashboard designed

--- UniversityAttendanceApp/components/Dashboard.js
+++ UniversityAttendanceApp/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, StatusBar } from 'react-native';
 import { SearchBar, Card, Text, Icon } from 'react-native-elements';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 import { Table, Row, Rows } from 'react-native-table-component';
@@ -26,8 +26,44 @@ const Dashboard = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <SearchBar placeholder="Search..." lightTheme round />
+      <SearchBar
+        placeholder="Search..."
+        lightTheme
+        containerStyle={{
+          backgroundColor: 'transparent',
+          borderTopWidth: 0, // Remove the top border
+          borderBottomWidth: 0, // Remove the bottom border
+        }}
+        // inputContainerStyle={{ backgroundColor: 'transparent' }}
+        searchIcon={{ color: 'black' }} // Set the search icon color as needed
+        clearIcon={{ color: 'black' }} // Set the clear icon color as needed
+      />
 
+      <Card containerStyle={styles.tableCard}>
+        <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+          <Row data={tableHead} style={styles.head} textStyle={styles.headText} />
+          <Rows data={tableData} textStyle={styles.text} />
+        </Table>
+      </Card>
+
+      <Card containerStyle={styles.dashboardCard}>
+
+      <Card containerStyle={styles.dashboardHeader}>
+        <View style={{flexDirection:'row', justifyContent:'space-between', height:'100%'}}>
+      <Icon
+                  name={'user'}
+                  type="font-awesome"
+                  size={15}
+                  color="#517fa4"
+                />
+                    <Icon
+                  name={'user'}
+                  type="font-awesome"
+                  size={20}
+                  color="#517fa4"
+                />
+                </View>
+</Card>
       <View style={styles.gridContainer}>
         {gridData.map((row, rowIndex) => (
           <View style={styles.gridRow} key={rowIndex}>
@@ -45,13 +81,9 @@ const Dashboard = () => {
           </View>
         ))}
       </View>
-
-      <Card containerStyle={styles.tableCard}>
-        <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
-          <Row data={tableHead} style={styles.head} textStyle={styles.headText} />
-          <Rows data={tableData} textStyle={styles.text} />
-        </Table>
       </Card>
+
+      <StatusBar backgroundColor='#5E27E7' />
     </ScrollView>
   );
 };
@@ -59,7 +91,8 @@ const Dashboard = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    padding: 10,
+    // padding: 10,
+    // backgroundColor: '#5E27E7'
   },
   gridContainer: {
     flexDirection: 'column',
@@ -72,12 +105,35 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   card: {
-    width: '46%',
+    width: '40%',
     alignItems: 'center',
+    borderRadius:14,
   },
   tableCard: {
     marginTop: 20,
     padding: 10,
+  },
+  dashboardCard: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '45%', // 25% of the screen's height
+    borderTopLeftRadius: 20, // Adjust the radius as needed
+    borderTopRightRadius: 20, // Adjust the radius as needed
+    backgroundColor: '#6495ED',
+    margin: 0, // Reset margin to zero
+    padding: 0, // Reset padding to zero
+  },
+  dashboardHeader:{
+    height: '15%', // 25% of the screen's height
+    width:"90%",
+    backgroundColor:'#40E0D0',
+    borderRadius:14,
+    marginTop:-30,
+    alignSelf:'center',
+   
+
   },
   head: { height: 40, backgroundColor: '#f1f8ff' },
   headText: { margin: 6, fontWeight: 'bold' },
--- UniversityAttendanceApp/components/Tabs.js
+++ UniversityAttendanceApp/components/Tabs.js
@@ -1,43 +1,70 @@
+import React, { lazy, Suspense } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Dashboard from './Dashboard';
-import Dashboard2 from './Dashboard2';
+import { Text } from 'react-native-paper';
+
 const Tab = createBottomTabNavigator();
 
-export default Tabs = ()=> {
+const LazyDashboard = lazy(() => import('./Dashboard'));
+const LazyDashboard2 = lazy(() => import('./Dashboard2'));
+
+const Tabs = () => {
   return (
     <Tab.Navigator
       initialRouteName="home"
       screenOptions={{
         tabBarActiveTintColor: '#e91e63',
-        headerShown:false
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#6495ED',
+      },
+      
       }}
       tabBarOptions={{
-        showLabel:false
-
+        showLabel: false,
       }}
+      tabBarStyle={{
+        backgroundColor: 'red', // Set your desired background color
+      }}
+     
     >
       <Tab.Screen
         name="Dashboard"
-        component={Dashboard}
         options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
         }}
-      />
-      
+      >
+        {() => (
+          <Suspense fallback={<LoadingComponent />}>
+            <LazyDashboard />
+          </Suspense>
+        )}
+      </Tab.Screen>
+
       <Tab.Screen
         name="Dashboard2"
-        component={Dashboard2}
         options={{
           tabBarLabel: 'Home2',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home2" color={color} size={size} />
           ),
         }}
-      />
+      >
+        {() => (
+          <Suspense fallback={<LoadingComponent />}>
+            <LazyDashboard2 />
+          </Suspense>
+        )}
+      </Tab.Screen>
     </Tab.Navigator>
   );
+};
+
+export default Tabs;
+
+function LoadingComponent() {
+  return <Text>Loading...</Text>; // Replace with your loading indicator component
 }
